81e3cb4f5f5eed0e95bea394012260fbd6e31e39
leocavalcante <lc@leocavalcante.com>
2019-09-17 17:07:51 -0300

    Só faltou mudar a sessão quando muda via AJAX

--- www/ajax.php
+++ www/ajax.php
@@ -7,6 +7,7 @@ $method = $_SERVER['REQUEST_METHOD'];
 if ($method === 'POST') {
     $lang = trim(filter_input(INPUT_POST, 'value'));
     $lang = $lang ?? 'pt';
+    $_SESSION['lg'] = $lang;
     $messages = file_get_contents(__DIR__ . "/../messages/$lang.json");
 
     header('Content-type: application/json');
